b1d54b246fe68dc8e7684c30c3aa9bee2a98dd36
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-14 09:48:26 +0100

    added square function and tests

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,5 +1,14 @@
 const utils = require('./utils');
+const expect = require('chai').expect;
 
 it('should add two numbers', () => {
   let res = utils.add(33, 11);
+
+  expect(res).to.equal(44).to.be.a('number');
+});
+
+it ('should square number', () => {
+  let res = utils.square(3);
+
+  expect(res).to.equal(9).to.be.a('number');
 });
