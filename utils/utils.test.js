0cde6fd95253030bf5b0c640e2b523e9b8c3ea31
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-14 11:12:21 +0100

    added async test for existing functions

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -7,8 +7,48 @@ it('should add two numbers', () => {
   expect(res).to.equal(44).to.be.a('number');
 });
 
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).to.equal(7).to.be.a('number');
+    done();
+  });
+});
+
 it ('should square number', () => {
   let res = utils.square(3);
 
   expect(res).to.equal(9).to.be.a('number');
 });
+
+it ('should async square a number', (done) => {
+  utils.asyncSquare(3, (sum) => {
+    expect(sum).to.equal(9);
+    done();
+  });
+});
+
+it ('should set first name and last name', () => {
+  let user = {
+    location: 'London',
+    age: 29
+  };
+
+  let res = utils.setName(user, 'Hemesh Unka');
+
+  expect(res).to.include({
+    firstName: 'Hemesh',
+    lastName: 'Unka'
+  });
+});
+
+// it('should expect some values', () => {
+//   // expect([2,3,4]).to.not.include(5);
+//   expect({
+//     name: 'Hemesh',
+//     age: 29,
+//     location: 'London'
+//   }).to.include({
+//     age: 29
+//   })
+// });
+
