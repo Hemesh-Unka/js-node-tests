2eb7d72c7d01d53b95eeae2af7cece7d8cfad974
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-14 12:13:37 +0100

    testing express routes

--- server/server.js
+++ server/server.js
@@ -3,7 +3,24 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.status(200).send([{
+    name: 'Hemesh',
+    age: 29
+  }, {
+    name: 'FakeGuy',
+    age: 12
+  }, { 
+    name: 'FakeGirl',
+    age: 200
+  }]);
 });
 
 app.listen(3000);
+module.exports.app = app;
