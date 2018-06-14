dd77de0502dbe71258ab4755639a098eeb6e08a3
Hemesh-Unka <hemeshunka@gmail.com>
2018-06-14 12:25:24 +0100

    updating tests descriptions

--- server/server.test.js
+++ server/server.test.js
@@ -2,28 +2,34 @@ const request = require('supertest');
 const app = require('./server').app;
 const expect = require('chai').expect;
 
-it('should return hello world as a response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).to.include({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
-});
-
-it('should return an array with my name in it', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).to.be.a('array')
-      expect(res.body).to.deep.include({
-        name: 'Hemesh',
-        age: 29
-      })
-    })
-  .end(done);
+describe('Server', () => {
+  describe('GET /users', () => {
+    it('should return an array with my name in it', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).to.be.a('array')
+          expect(res.body).to.deep.include({
+            name: 'Hemesh',
+            age: 29
+          })
+        })
+      .end(done);
+    });
+  });
+  
+  describe('GET /', () => {
+    it('should return hello world as a response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).to.include({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,54 +1,55 @@
 const utils = require('./utils');
 const expect = require('chai').expect;
 
-it('should add two numbers', () => {
-  let res = utils.add(33, 11);
+describe('Utils', () => {
+  it('should add two numbers', () => {
+    let res = utils.add(33, 11);
+  
+    expect(res).to.equal(44).to.be.a('number');
+  });
 
-  expect(res).to.equal(44).to.be.a('number');
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).to.equal(7).to.be.a('number');
-    done();
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).to.equal(7).to.be.a('number');
+      done();
+    });
+  });
+  
+  it ('should square number', () => {
+    let res = utils.square(3);
+  
+    expect(res).to.equal(9).to.be.a('number');
+  });
+  
+  it ('should async square a number', (done) => {
+    utils.asyncSquare(3, (sum) => {
+      expect(sum).to.equal(9);
+      done();
+    });
+  });
+  
+  it ('should set first name and last name', () => {
+    let user = {
+      location: 'London',
+      age: 29
+    };
+  
+    let res = utils.setName(user, 'Hemesh Unka');
+  
+    expect(res).to.include({
+      firstName: 'Hemesh',
+      lastName: 'Unka'
+    });
   });
 });
 
-it ('should square number', () => {
-  let res = utils.square(3);
-
-  expect(res).to.equal(9).to.be.a('number');
-});
-
-it ('should async square a number', (done) => {
-  utils.asyncSquare(3, (sum) => {
-    expect(sum).to.equal(9);
-    done();
-  });
-});
-
-it ('should set first name and last name', () => {
-  let user = {
-    location: 'London',
-    age: 29
-  };
-
-  let res = utils.setName(user, 'Hemesh Unka');
-
-  expect(res).to.include({
-    firstName: 'Hemesh',
-    lastName: 'Unka'
-  });
-});
-
-// it('should expect some values', () => {
-//   // expect([2,3,4]).to.not.include(5);
-//   expect({
-//     name: 'Hemesh',
-//     age: 29,
-//     location: 'London'
-//   }).to.include({
-//     age: 29
-//   })
-// });
-
+  // it('should expect some values', () => {
+  //   // expect([2,3,4]).to.not.include(5);
+  //   expect({
+  //     name: 'Hemesh',
+  //     age: 29,
+  //     location: 'London'
+  //   }).to.include({
+  //     age: 29
+  //   })
+  // });
